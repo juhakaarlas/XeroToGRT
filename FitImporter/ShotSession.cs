3dfcf61286d82211452a819899b4c34305c2f00c
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2024-05-09 21:52:10 +0200

    Add statistics

--- FitImporter/ShotSession.cs
+++ FitImporter/ShotSession.cs
@@ -13,18 +13,89 @@
 
     public class ShotSession
     {
+        private const double InvalidSpeed = -1;
+
         public int Id { get; set; }
 
         public DateTime StartTime { get; set; }
 
         public int ShotCount => Shots.Count;
 
-        public float AvgSpeed { get; set; }
+        private double _avgSpeed = InvalidSpeed;
 
-        public float MinSpeed { get; set; }
+        public double AvgSpeed {
+            get
+            {
+                if (_avgSpeed == InvalidSpeed)
+                {
+                    _avgSpeed = CalculateAvgSpeed();
+                }
 
-        public float MaxSpeed { get; set; }
+                return _avgSpeed;
+            }
+        }
+
+        private double _stdDev = InvalidSpeed;
+
+        public double StdDev
+        {
+            get
+            {
+                if (_stdDev == InvalidSpeed)
+                {
+                    _stdDev = CalculateStdDev();
+                }
+                return _stdDev;
+            }
+        }
+
+        private double _minSpeed = InvalidSpeed;
+
+        public double MinSpeed
+        {
+            get
+            {
+                if (_minSpeed == InvalidSpeed)
+                {
+                    if (Shots == null) { return InvalidSpeed; }
+
+                    _minSpeed = Shots.Min(s =>  s.Speed);
+                }
+
+                return _minSpeed;
+            }
+        }
+
+        private double _maxSpeed = InvalidSpeed;
+
+        public double MaxSpeed
+        {
+            get
+            {
+                if (_maxSpeed == InvalidSpeed)
+                {
+                    if (Shots == null) { return InvalidSpeed; }
+                    _maxSpeed = Shots.Max(s => s.Speed);
+                }
+
+                return _maxSpeed;
+            }
+        }
 
         public List<Shot>? Shots { get; set; }
+
+        private double CalculateAvgSpeed()
+        {
+            if (Shots == null) { return InvalidSpeed; }
+
+            return Shots.Average(s => s.Speed);
+        }
+
+        private double CalculateStdDev()
+        {
+            if (Shots == null) { return InvalidSpeed; }
+
+            return Math.Sqrt(Shots.Average(s => Math.Pow(s.Speed - AvgSpeed, 2)));
+        }
     }
 }
