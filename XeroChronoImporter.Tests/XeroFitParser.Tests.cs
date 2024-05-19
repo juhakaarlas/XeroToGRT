88fa615ead58ba985d38faabb0ae99b8f056d2e1
Juha Kaarlas <11892788+juhakaarlas@users.noreply.github.com>
2024-05-19 23:46:26 +0200

    Test class for FIT parser

---
+++ XeroChronoImporter.Tests/XeroFitParser.Tests.cs
@@ -0,0 +1,16 @@
+﻿namespace XeroChronoImporter.Tests
+{
+    public class XeroFitParserTests
+    {
+        private const string TestFitFile = "xero-test.fit";
+
+        [Fact]
+        public void CanReadFit()
+        {
+            var testee = new XeroFitParser();
+            var actual = testee.Process(TestUtils.GetTestDataFilePath(TestFitFile));
+
+            Assert.NotNull(actual);
+        }
+    }
+}
